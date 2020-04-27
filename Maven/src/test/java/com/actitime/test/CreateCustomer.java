d72fd2152d8b74059da2ee991950650883972993
sarvesh <sarveshmahajan@gmail.com>
2020-04-27 15:55:24 +0530

    commitnew

---
+++ Maven/src/test/java/com/actitime/test/CreateCustomer.java
@@ -0,0 +1,13 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	
+	@Test
+	public void testCreateCustomer() {
+		Reporter.log("CreateCustomer", true);
+	}
+
+}
